819ac87c7d5473eda7b252b774ecfbb9c58cb88c
colin <colin@localhost>
2022-11-01 15:35:05 -0400

    1 Novembre 2022

---
+++ Assignment/src/B001.java
@@ -0,0 +1,9 @@
+public class B001 {
+
+	public static void main (String[] args)
+	{
+		int age = 16;
+		System.out.println (age);
+	}
+	// main method
+}
---
+++ Assignment/src/B002.java
@@ -0,0 +1,10 @@
+public class B002 
+{
+	public static void main (String[] args)
+	{
+		int age = 0;
+		System.out.println ("Ton age est " + age);
+		age = 16;
+		System.out.println ("Ton age est maintenant " + age);
+	}
+}
