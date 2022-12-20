f371f4938223243ac1852f0458fa94fb407f7217
colin <colin@localhost>
2022-12-20 15:14:31 -0500

    devoir du 20

--- Assignment/src/toto.java
+++ Assignment/src/toto.java
@@ -1,4 +1,14 @@
-
 public class toto {
 
-}
+	  public static void main(String[] args) {
+		  
+	    String text = "les chaussettes de l'archiduchesse sont-elles sèches ou archisèches";
+	    int count = 0;
+	    int index = text.indexOf("ch");
+	    while (index != -1) {
+	      count++;
+	      index = text.indexOf("ch", index + 1);
+	    }
+	    System.out.println("The string \"ch\" appears " + count + " times in the text.");
+	  }
+	}
